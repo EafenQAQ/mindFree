2dde84dbff3475e87f682acae0478dc290b4e0d9
EafenQAQ <eafenqaq@gmail.com>
2025-03-04 19:17:10 +0800

    增加条幅

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,12 @@
     <section class="heroArea relative h-[90vh] flex items-center">
       <HeroArea />
     </section>
+    <!-- 条幅简介 -->
+    <section class="Banner mt-10">
+      <div class="bg-gradient-to-tr from-[#c0edf9] to-[#4bf2df] h-30 flex items-center justify-center font-semibold text-white text-xl">
+        <h3>EaseFree是一款自助AI工具，当你感到担忧、悲伤、压力或单纯想要倾诉时可以使用。它就像一个可以倾听你烦恼并帮助你缓解情绪的朋友。</h3>
+      </div>
+    </section>
   </main>
 
   <img src="../assets/R.jpg" alt="bgImg">

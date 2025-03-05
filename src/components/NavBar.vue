5de91dd53995eb3d5a0fbf2a2575777156d6273b
EafenQAQ <eafenqaq@gmail.com>
2025-03-05 16:38:10 +0800

    change name to "mindFree" instead of "EaseFree"

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
         <div class="flex items-center justify-between h-16">
             <a href="/" class="flex bg-white py-2 rounded-full items-center router-link-active router-link-exact-active btn btn-ghost p-0 m-0 h-fit w-fit hover:bg-gray-200">
                 <img src="../assets/logo.webp" alt="navLogo" class="h-8 ml-1 bg-blue-gray-50 rounded-full p-1">
-                <span class="logoText mr-4 font-semibold">EaseFree.AI</span>
+                <span class="logoText mr-4 font-semibold">mindFree.AI</span>
             </a>
             <div class="signin">
                 <button class="relative inline-flex items-center justify-center w-20 transition-all duration-300 ease-in-out bg-gradient-to-tr from-[#8bdaf0] to-blue-500 hover:from-[#62a9bd] hover:to-blue-600 text-white font-semibold py-2 px-4 rounded-full hover:shadow-lg">登录</button>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
     <!-- 条幅简介 -->
     <section class="Banner mt-10">
       <div class="bg-gradient-to-tr from-[#c0edf9] to-[#4bf2df] h-30 flex items-center justify-center font-semibold text-white text-xl">
-        <h3>EaseFree是一款自助AI工具，当你感到担忧、悲伤、压力或单纯想要倾诉时可以使用。它就像一个可以倾听你烦恼并帮助你缓解情绪的朋友。</h3>
+        <h3>mindFree是一款自助AI工具，当你感到担忧、悲伤、压力或单纯想要倾诉时可以使用。它就像一个可以倾听你烦恼并帮助你缓解情绪的朋友。</h3>
       </div>
     </section>
   </main>
